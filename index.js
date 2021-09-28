f5a800547523a6fc158b499109b30ceec66ee265
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2021-09-28 10:40:03 +0530

    Update index.js
    new question added

--- index.js
+++ index.js
@@ -7,41 +7,50 @@
 
   // play function
 
-  function play(question,answer) {
-  var userAnswer = readlineSync.question(question);
+  function play(question, answer) {
+    var userAnswer = readlineSync.question(question);
 
-  if (userAnswer === answer) {
-    console.log("right!");
-    score = score + 1;
+    if (userAnswer === answer) {
+      console.log("right!");
+      score = score + 1;
 
-  } else {
-    console.log("wrong!")
-  }
+    } else {
+      console.log("wrong!")
+    }
 
-  console.log("current score: ", score);
-  console.log("----------------------")
+    console.log("current score: ", score);
+    console.log("----------------------")
 
   }
 
   // array of objects
   var questions = [{
-    question: "Where do I live? ",
-    answer: "Pune"
-  },  {
-    question: "My Home Town? ",
-    answer: "Ranchi"
-  },{
-    question:"Which month is my birthday ",
-    answer:"August"
+      question: "Where do I live? ",
+      answer: "Pune",
+    },
+    {
+      question: "My Home Town? ",
+      answer: "Ranchi",
+    },
+    {
+      question: "Which month is my birthday ",
+      answer: "August",
+    },
+    {
+      question: "I like Mountains/Beaches ?",
+      answer: "Beaches"
+    },
 
-  },{question: "I like Mountains/Beaches ?",
-  answer: "Beaches"
-  }];
+    {
+      question: "What's my Favourite Web Series?",
+      answer: "Suits"
+    },
+  ];
 
   // loop
-  for (var i=0; i<questions.length; i++) {
+  for (var i = 0; i < questions.length; i++) {
     var currentQuestion = questions[i];
     play(currentQuestion.question, currentQuestion.answer)
   }
 
-  console.log("YAY! you SCORED:  " ,score)
+  console.log("YAY! you SCORED:  ", score)
